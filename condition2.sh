1301544f8c4d021f8b92835e7d25415510cae4d2
manojkumar <thipparapumanojkumar15@gmail.comgit config --global user.name manojkumaromit --globalcleargit config --global user.email thipparapumanojkumar15@gmail.com>
2025-01-06 14:19:17 +0530

    updated script

--- condition2.sh
+++ condition2.sh
@@ -9,3 +9,21 @@ then
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+   echo "installation of postfix is error"
+   exit 1
+else
+   echo "installation of postfix is success"
+fi
+
+yum install maven -y
+
+if [ $? -ne 0 ]
+then 
+   echo "installation of maven is error"
+   exit 1
+else
+   echo "installation of maven is success"
+fi       
