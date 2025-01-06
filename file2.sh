be71e5fd574b581f4e1849d196f094cd3198d15d
manojkumar <thipparapumanojkumar15@gmail.comgit config --global user.name manojkumaromit --globalcleargit config --global user.email thipparapumanojkumar15@gmail.com>
2025-01-06 12:43:14 +0530

    updated script

--- file2.sh
+++ file2.sh
@@ -5,3 +5,9 @@ echo "please enter your username"
 read username
 
 echo "username entered is: $username"
+
+echo "please enter your password"
+
+read password
+
+echo "pasword entered is: $password"
